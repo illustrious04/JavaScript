26480ad539d85327c8cca1ca1f3bdabd082a0558
illustrious04 <118503766+illustrious04@users.noreply.github.com>
2024-10-19 12:53:30 +0000

    Datypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,12 @@
+"use strict" // Treat all JS code as newer version.
+
+// alert (3+3) We are using Node not; Browser 
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefiend
+// symbol
+// Non Primitive= object
